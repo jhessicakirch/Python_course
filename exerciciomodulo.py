05ded0d41849f12ac5bcbe56934f2f4d449ab92b
Jhessica Kirch <95087779+jhessicakirch@users.noreply.github.com>
2024-11-13 17:19:04 -0300

    Add files via upload

---
+++ exerciciomodulo.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 13 17:09:11 2024
+
+@author: jhess
+"""
+
+def texto(m):
+  '''
+  Retorna o texto digitado como parametro
+  '''
+  return input(m)
+
+def  numero(n):
+   return float(input(n))
---
+++ utilitarios.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 13 17:03:59 2024
+
+@author: jhess
+"""
+
+def soma(a, b, c):
+    somatorio = a + b + c
+    return somatorio
